c9ec13da7a407cd0ef736c62ba699508c1efe405
Nicolas <nicolascamara29@gmail.com>
2018-11-09 22:05:27 -0500

    22

---
+++ Bad-Employee/Assets/StopAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAnimation : MonoBehaviour {
+
+    //public AnimationClip anim;
+
+    public void StopAnimationn()
+    {
+        GetComponent<Animator>().enabled = false;
+    }
+}
